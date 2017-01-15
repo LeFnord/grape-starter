f69a5dec34ac2a40b086a3036800c534fb76e3f8
LeFnord <pscholz.le@gmail.com>
2017-01-15 22:10:50 +0100

    minor code moving

--- template/config/application.rb
+++ template/config/application.rb
@@ -4,24 +4,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'api'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'boot'
-
-Bundler.require :default, ENV['RACK_ENV']
-
-Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each do |lib|
-  require lib
-end
-
-Dir[File.expand_path('../../api/entities/*.rb', __FILE__)].each do |entity|
-  require entity
-end
-
-Dir[File.expand_path('../../api/endpoints/*.rb', __FILE__)].each do |endpoint|
-  require endpoint
-end
-
 require 'base'
-
 require 'rack'
+
 # provides the documentation of the API
 class DocApp
   attr_reader :env
--- template/config/boot.rb
+++ template/config/boot.rb
@@ -1,3 +1,17 @@
 # frozen_string_literal: true
 require 'rubygems'
 require 'bundler/setup'
+
+Bundler.require :default, ENV['RACK_ENV']
+
+Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each do |lib|
+  require lib
+end
+
+Dir[File.expand_path('../../api/entities/*.rb', __FILE__)].each do |entity|
+  require entity
+end
+
+Dir[File.expand_path('../../api/endpoints/*.rb', __FILE__)].each do |endpoint|
+  require endpoint
+end
