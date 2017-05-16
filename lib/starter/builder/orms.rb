dd7855921ac5b25388a84dcbcbe32b9b5d87f53c
LeFnord <pscholz.le@gmail.com>
2017-05-16 22:36:15 +0200

    adds base storing of configuration

--- lib/starter/builder/orms.rb
+++ lib/starter/builder/orms.rb
@@ -17,17 +17,18 @@ module Starter
         append_to_file(File.join(dest, 'Rakefile'), rakefile)
         append_to_file(File.join(dest, 'Gemfile'), gemfile)
         prepare_for_migrations(File.join(dest, 'db'))
+        Starter::Config.save(dest: dest, content: { orm: orm })
       end
 
       private
 
-      def build_initializer(new_dest)
-        FileUtils.mkdir_p(new_dest)
-        FileFoo.write_file(File.join(new_dest, 'database.rb'), initializer)
+      def build_initializer(dest)
+        FileUtils.mkdir_p(dest)
+        FileFoo.write_file(File.join(dest, 'database.rb'), initializer)
       end
 
-      def build_config(new_dest)
-        FileFoo.write_file(File.join(new_dest, 'database.yml'), config)
+      def build_config(dest)
+        FileFoo.write_file(File.join(dest, 'database.yml'), config)
       end
 
       def append_to_file(file_name, content)
@@ -35,8 +36,8 @@ module Starter
         FileFoo.write_file(file_name, "#{original}\n\n#{content}")
       end
 
-      def prepare_for_migrations(new_dest)
-        migrations = File.join(new_dest, 'migrations')
+      def prepare_for_migrations(dest)
+        migrations = File.join(dest, 'migrations')
         FileUtils.mkdir_p(migrations)
         `touch #{migrations}/.keep`
       end
