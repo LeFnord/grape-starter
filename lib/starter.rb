31bfb0d4b19029d0518f02be442492b97712a02b
LeFnord <pscholz.le@gmail.com>
2023-02-13 13:50:01 +0100

    Minor clean up.

--- lib/starter.rb
+++ lib/starter.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 require 'awesome_print'
-
 require 'active_support'
 require 'active_support/core_ext/string'
-
-# require 'starter/version'
-# require 'starter/builder'
-# require 'starter/config'
-
 require 'zeitwerk'
+
 loader = Zeitwerk::Loader.for_gem
 loader.setup
 
--- lib/starter/build.rb
+++ lib/starter/build.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module Starter
-  # require 'starter/builder/orms'
-  # require 'starter/builder/names'
-  # require 'starter/builder/base_file'
-  # require 'starter/builder/file_foo'
-  # require 'starter/builder/templates/files'
-  # require 'starter/builder/templates/endpoints'
-
   class Build
     extend Builder::Names
     extend Builder::BaseFile
